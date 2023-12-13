6406cacd2e18012043ef4adaaf9abbbb9481b2f4
Ashan Ghimire <ashanghimire10@gmail.com>
2023-12-13 21:29:31 +0545

    Neovim initial setup

---
+++ init.lua
@@ -0,0 +1,2 @@
+require("aashan")
+
---
+++ lua/aashan/packer.lua
@@ -0,0 +1,108 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd.packadd('packer.nvim')
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+
+  use({
+      "folke/trouble.nvim",
+      config = function()
+          require("trouble").setup {
+              icons = false,
+          }
+      end
+  })
+
+  use {
+			'nvim-treesitter/nvim-treesitter',
+			run = function()
+				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+				ts_update()
+			end,}
+  use("nvim-treesitter/playground")
+  use("theprimeagen/harpoon")
+  use("theprimeagen/refactoring.nvim")
+  use("mbbill/undotree")
+  use("nvim-tree/nvim-tree.lua")
+  use("rebelot/terminal.nvim")
+  use("tpope/vim-fugitive")
+  use("windwp/nvim-autopairs")
+  use {"github/copilot.vim", branch = 'release'}
+  use {"akinsho/bufferline.nvim", requires = "nvim-tree/nvim-web-devicons"}
+
+  use {
+      "ryanoasis/vim-devicons",
+      config = function()
+          vim.cmd [[ set termguicolors ]]
+          require("nvim-web-devicons").setup {
+              override = {
+                  zsh = {
+                      icon = "",
+                      color = "#428850",
+                      name = "Zsh"
+                  }
+              };
+              default = true;
+          }
+      end
+  }
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},
+		  {'rafamadriz/friendly-snippets'},
+	  }
+  }
+
+  use("folke/zen-mode.nvim")
+  use("eandrju/cellular-automaton.nvim")
+  use("laytan/cloak.nvim")
+
+end)
+
